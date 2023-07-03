9b3f6b5518d539b7f03730eeff7c7fc76dc057dd
kethnal <kaseyvaldez@hotmail.com>
2023-07-03 04:03:26 -0700

    PLEASE

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -45,9 +45,6 @@ import { useEffect } from 'react'
 export default function Error({
   error,
   reset,
-}: {
-  error: Error
-  reset: () => void
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
