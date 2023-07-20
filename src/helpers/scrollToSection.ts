86440d9b884707cc41febecc85d691421375f6b1
kethnal <kaseyvaldez@hotmail.com>
2023-07-20 02:03:25 -0700

    Move menu nav styling to own file and implement active styling on navitems to corresponding section in viewport

--- src/helpers/scrollToSection.ts
+++ src/helpers/scrollToSection.ts
@@ -2,11 +2,11 @@
 
 export function scrollToSection(id: string) {
   const isMobile = window.innerWidth <= 768;
-
   const element = document.getElementById(id);
+
   if (element) {
-    const navbarHeight = isMobile ? 70 : 160; // Height of your navbar in pixels
+    const navbarHeight = isMobile ? 70 : 155; // navbar height in pixels
     const offset = element.offsetTop - navbarHeight;
-    window.scrollTo({ top: offset, behavior: 'smooth' });
+    window.scrollTo({ top: offset, behavior: "smooth" });
   }
-};
+}
